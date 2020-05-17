8725190d2f88fab65d1f9874fcda93af87415922
MichaelMontgom <mem22695@gmail.com>
2020-05-16 23:47:08 -0400

    working on getting ingredients to show up on create

--- recipes/main/models.py
+++ recipes/main/models.py
@@ -2,18 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Recipe(models.Model):
-    name = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    instructions = models.TextField()
-    serving_size = models.IntegerField()
-
-
-    def __str__(self):
-        return self.name
 
 class Ingredients(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField( max_length=100)
     measurement_size = models.FloatField()
     measurement_type = models.CharField(max_length=20)
@@ -21,3 +11,18 @@ class Ingredients(models.Model):
     
     def __str__(self):
         return f'{measurement_size} {measurement_type} {name}'
+
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    ingredients = models.ManyToManyField(Ingredients)
+    instructions = models.TextField()
+    serving_size = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
--- recipes/main/views.py
+++ recipes/main/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import CreateNewRecipe
+from .models import Recipe, Ingredients
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 def index(response):
     return render(response, 'main/home.html')
+
+
+def create(response):
+    if response.method == 'POST':
+        form = CreateNewRecipe(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            a = form.cleaned_data['author']
+            instructions = form.cleaned_data['instructions']
+            serving_size = form.cleaned_data['serving_size']
+
+            r = Recipe(name=n, author=a, instructions=instructions, serving_size=serving_size)
+            r.save()
+            return HttpResponseRedirect('%i' %r.id)
+    else:
+        form = CreateNewRecipe()
+    return render(response, 'main/create.html', {'form': form})
+
+
+

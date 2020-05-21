c5aa0a84645412ec94a52b8aaa4ea89496770974
MichaelMontgom <mem22695@gmail.com>
2020-05-20 20:36:25 -0400

    setup class based views and respective html pages

--- recipes/main/views.py
+++ recipes/main/views.py
@@ -1,30 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
+
 from .forms import CreateNewRecipe
 from .models import Recipe, Ingredients
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
 # Create your views here.
 
-def index(response):
-    return render(response, 'main/home.html')
+# def index(response):
+#     return render(response, 'main/home.html')
 
 
-def create(response):
-    if response.method == 'POST':
-        form = CreateNewRecipe(response.POST)
+# def home(response):
+#
+#     recipes = Recipe.objects.all()
+#
+#     return render(response, "main/home.html", {'recipes': recipes})
+#
+#
+# def create(response):
+#     if response.method == 'POST':
+#         form = CreateNewRecipe(response.POST)
+#
+#         if form.is_valid():
+#             n = form.cleaned_data['name']
+#             a = form.cleaned_data['author']
+#             instructions = form.cleaned_data['instructions']
+#             serving_size = form.cleaned_data['serving_size']
+#
+#             r = Recipe(name=n, author=a, instructions=instructions, serving_size=serving_size)
+#             r.save()
+#             return redirect('/home')
+#     else:
+#         form = CreateNewRecipe()
+#     return render(response, 'main/create.html', {'form': form})
 
-        if form.is_valid():
-            n = form.cleaned_data['name']
-            a = form.cleaned_data['author']
-            instructions = form.cleaned_data['instructions']
-            serving_size = form.cleaned_data['serving_size']
+class RecipesList(ListView):
+    model = Recipe
 
-            r = Recipe(name=n, author=a, instructions=instructions, serving_size=serving_size)
-            r.save()
-            return HttpResponseRedirect('%i' %r.id)
-    else:
-        form = CreateNewRecipe()
-    return render(response, 'main/create.html', {'form': form})
+class RecipesView(DetailView):
+    model = Recipe
 
+class RecipesCreate(CreateView):
+    model = Recipe
+    fields = ['name', 'author', 'instructions', 'serving_size']
+    success_url = reverse_lazy('recipes_list')
 
+class RecipesUpdate(UpdateView):
+    model = Recipe
+    fields = ['name', 'author', 'instructions', 'serving_size']
+    success_url = reverse_lazy('recipes_list')
 
+class RecipesDelete(DeleteView):
+    model = Recipe
+    success_url = reverse_lazy('recipes_list')
